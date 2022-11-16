affbc485439029837529b0aba66004947166d7bb
amnahkhatun <amna.khatun1605@gmail.com>
2022-11-16 15:15:19 +0530

    worked in single product pages and stars

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
-import { Home, About, Cart, Checkout, Error, PrivateRoute, Products, SingleProduct } from './pages'
+
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+  // AuthWrapper,
+} from './pages'
 
 function App() {
   return (
+    // <AuthWrapper>
     <Router>
       <Navbar />
       <Sidebar />
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <Home />
         </Route>
-        <Route exact path="/about">
+        <Route exact path='/about'>
           <About />
         </Route>
-        <Route exact path="/cart">
+        <Route exact path='/cart'>
           <Cart />
         </Route>
-        <Route exact path="/checkout">
-          <Checkout />
-        </Route>
-        <Route exact path="/products">
+        <Route exact path='/products'>
           <Products />
         </Route>
-        <Route exact path="/products:id" children={<SingleProduct />} />
-        <Route exact path="*">
+        <Route exact path='/products/:id' children={<SingleProduct />} />
+        <PrivateRoute exact path='/checkout'>
+          <Checkout />
+        </PrivateRoute>
+        <Route path='*'>
           <Error />
         </Route>
       </Switch>
       <Footer />
-    </Router >
-
+    </Router>
+    // </AuthWrapper>
   )
 }
 
